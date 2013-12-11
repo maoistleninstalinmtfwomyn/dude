e8fd4d6aab8d3bf6ed30a8ca8c2277a0e61afb79
Alberto Corona <albcoron@gmail.com>
2013-12-10 19:06:37 -0600

    Initial commit Welcome to dude v1.0.1
    Signed-off-by: Alberto Corona <albcoron@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# dude
+You can build some really great, fast, powerful frameworks using dude. As the maintainer of dude, I might have some bias, but I build dude because there was nothing else out there that did what it did. They, them, he, she, user, you, none of those gave me the results of dude.
+
+## Using dude
+To use dude run ./dude.sh <directory to apply dude>
+
+
+[Relevent](https://github.com/joyent/libuv/pull/1015)
---
+++ dude.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sed -i -e 's/They/dude/g' $1
+sed -i -e 's/they/dude/g' $1
+sed -i -e 's/Them/dude/g' $1
+sed -i -e 's/them/dude/g' $1
+sed -i -e 's/He/dude/g' $1
+sed -i -e 's/he/dude/g' $1
+sed -i -e 's/Him/dude/g' $1
+sed -i -e 's/him/dude/g' $1
+sed -i -e 's/She/dude/g' $1
+sed -i -e 's/she/dude/g' $1
+sed -i -e 's/Her/dude/g' $1
+sed -i -e 's/her/dude/g' $1
+sed -i -e 's/You/dude/g' $1
+sed -i -e 's/you/dude/g' $1
+sed -i -e 's/User/dude/g' $1
+sed -i -e 's/user/dude/g' $1
