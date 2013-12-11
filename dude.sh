3e1f7047e9df201af472c4aa428c4db337a4e971
maoistleninstalinmtfwomyn <c2542780@drdrb.com>
2013-12-10 20:15:58 -0500

    Changed 'dude' to a more politically correct name.

--- dude.sh
+++ dude.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sed -i -e 's/They/dude/g' $1
-sed -i -e 's/they/dude/g' $1
-sed -i -e 's/Them/dude/g' $1
-sed -i -e 's/them/dude/g' $1
-sed -i -e 's/He/dude/g' $1
-sed -i -e 's/he/dude/g' $1
-sed -i -e 's/Him/dude/g' $1
-sed -i -e 's/him/dude/g' $1
-sed -i -e 's/She/dude/g' $1
-sed -i -e 's/she/dude/g' $1
-sed -i -e 's/Her/dude/g' $1
-sed -i -e 's/her/dude/g' $1
-sed -i -e 's/You/dude/g' $1
-sed -i -e 's/you/dude/g' $1
-sed -i -e 's/User/dude/g' $1
-sed -i -e 's/user/dude/g' $1
+sed -i -e 's/They/Xim/g' $1
+sed -i -e 's/they/xim/g' $1
+sed -i -e 's/Them/Xim/g' $1
+sed -i -e 's/them/xim/g' $1
+sed -i -e 's/He/Xer/g' $1
+sed -i -e 's/he/xer/g' $1
+sed -i -e 's/Him/Xer/g' $1
+sed -i -e 's/him/xer/g' $1
+sed -i -e 's/She/Xer/g' $1
+sed -i -e 's/she/xer/g' $1
+sed -i -e 's/Her/Xer/g' $1
+sed -i -e 's/her/xer/g' $1
+sed -i -e 's/You/Xim/g' $1
+sed -i -e 's/you/xim/g' $1
+sed -i -e 's/User/Xim/g' $1
+sed -i -e 's/user/xim/g' $1
